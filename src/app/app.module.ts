75c55614dcfc8afdc563dea9ec8a3dc052b7fad5
saadbinamjad <saadbinamjad@gmail.com>
2017-07-29 11:09:29 +0600

    Fixed tsconfig.server file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-     BrowserModule.withServerTransition({appId: 'universal-demo'}),
+     BrowserModule.withServerTransition({appId: 'my-app'}),
   ],
   providers: [],
   bootstrap: [AppComponent]
